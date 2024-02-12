814b7bec483b384a5abc2e589c56e920dc14afcc
Adam Hradil <hradilad@gmail.com>
2024-02-12 20:42:08 +0100

    fix xpaths, change default url params, output results to json

--- bezrealitky/spiders/search_flats.py
+++ bezrealitky/spiders/search_flats.py
@@ -1,7 +1,9 @@
 import scrapy
 from urllib.parse import urlencode
-from ..items import BezrealitkyItem
-from ..settings import is_passing
+import json
+from pydispatch import dispatcher
+import scrapy
+from scrapy import signals
 
 class SearchFlatsSpider(scrapy.Spider):
     name = "search_flats"
@@ -9,15 +11,21 @@ class SearchFlatsSpider(scrapy.Spider):
     params = [
         ('offerType', 'PRONAJEM'),
         ('estateType', 'BYT'),
-        ('disposition', 'DISP_2_KK'),
-        ('disposition', 'DISP_2_1'),
-        ('regionOsmIds', 'R439840'),
-        ('newBuilding', 'true'),
+        ('regionOsmIds', 'R435514'),
         ('order', 'PRICE_ASC'),
-        ('osm_value', 'Praha, okres Hlavní město Praha, Hlavní město Praha, Praha, Česko')
+        ('osm_value', 'Hlavní město Praha, Praha, Česko'),
+        ('currency', 'CZK')
     ]
     len_params = len(params)
-    start_urls = ["https://bezrealitky.cz/vypis?" + urlencode(params)]
+    start_urls = ["https://www.bezrealitky.cz/vypis?" + urlencode(params)]
+    listings = []
+
+    def __init__(self):
+        dispatcher.connect(self.spider_closed, signals.spider_closed)
+
+    def spider_closed(self):
+        with open('bezrealitky.json', mode='w', encoding='utf-8') as f:
+            json.dump(self.listings,fp=f, indent=2, ensure_ascii=False)
 
     def parse(self, response):
         yield from self.for_page(response)
@@ -26,24 +34,29 @@ class SearchFlatsSpider(scrapy.Spider):
                 self.params.append(('page', i))
             else:
                 self.params[self.len_params] = ('page', i)
-            yield scrapy.Request("https://bezrealitky.cz/vyhledat?" + urlencode(self.params))
+            yield scrapy.Request("https://www.bezrealitky.cz/vyhledat?" + urlencode(self.params))
 
     def for_page(self, response):
-        links = response.xpath("//h2[contains(@class, 'PropertyCard_propertyCardHeadline__y3bhA') and contains(@class, 'mt-4')]/a/@href").getall()
+        links = response.xpath("//h2[contains(@class, 'PropertyCard_propertyCardHeadline')]/a/@href").getall()
         for link in links:
             yield scrapy.Request(link, callback=self.filter_flats)
 
     def filter_flats(self, response):
-        sklonovani = ['mycka', 'mycky', 'mycky', 'mycek', 'mycce', 'myckam', 'mycku', 'mycky', 'mycce', 'myckach', 'myckou', 'myckami']
+        rent = response.xpath("//span[contains(text(), 'Měsíční nájemné')]/../../following-sibling::*/strong/span/text()").get()
+        service_fees = response.xpath("//span[contains(text(), '+ Poplatky za služby')]/../../following-sibling::*/strong/span/text()").get()
+        security_deposit = response.xpath("//span[contains(text(), '+ Vratná kauce')]/../../following-sibling::*/strong/span/text()").get()
+        address = response.xpath("//h1[contains(@class, 'mb-3')]/span[contains(@class, 'd-block')]/text()").get()
+        description = response.xpath("//div[contains(@class, 'box')]/p[contains(@class, 'text-perex-lg')]/text()").get()
+        disposition = response.xpath("//span[.='Dispozice']/../following-sibling::*/a/span/text()").get()
+        id = response.xpath("//span[.='Číslo inzerátu']/../following-sibling::*/text()").get()
+        available_from = response.xpath("//span[.='Dostupné od']/../following-sibling::*/span/text()").get()
+        floor = response.xpath("//span[.='Podlaží']/../following-sibling::*/span/text()").get()
+        type = response.xpath("//span[.='Typ budovy']/../following-sibling::*/span/text()").get()
+        area = response.xpath("//span[.='Plocha']/../following-sibling::*/span/text()").get()
+        furnished = response.xpath("//span[.='Vybaveno']/../following-sibling::*/span/text()").get()
+        status = response.xpath("//span[.='Stav']/../following-sibling::*/span/text()").get()
+        ownership = response.xpath("//span[.='Vlastnictví']/../following-sibling::*/span/text()").get()
+        penb = response.xpath("//span[.='PENB']/../following-sibling::*/span/text()").get()
+        design = response.xpath("//span[.='Provedení']/../following-sibling::*/span/text()").get()
 
-        item = BezrealitkyItem(penb=response.xpath(
-                "//div[@class='ParamsTable_paramsTableGroup__IIJ_u']//tr[th='PENB']/td/text()").get(),
-                               url=response.url,
-                               location=response.xpath("//h1/span/text()").get(),
-                               price=response.xpath(
-            "(//div[contains(@class, 'mb-lg-9') and contains(@class, 'mb-6')])[1]//strong[contains(@class, 'h4') and contains(@class, 'fw-bold')]/text()")
-            .get().replace('\xa0', ' '),
-                                has_washer=any(i in response.xpath("//p[@class='text-perex-lg']/text()").get().replace("č", "c").replace("á", "a")
-                                               for i in sklonovani)
-                                )
-        return item if is_passing(item) else None
+        self.listings.append(( response.url, rent, service_fees, security_deposit, address, description, disposition, id, available_from, floor, type, area, furnished, status, ownership, penb, design ))
